e53ca1a06d90cc6985eef054887b5e73b45e217c
Massimo Santini <massimo.santini@gmail.com>
2017-06-19 21:22:12 +0200

    Added cleanup

--- src/st/cmds/start.py
+++ src/st/cmds/start.py
@@ -8,7 +8,11 @@ from st import redis
 def main():
     parser = ArgumentParser(prog = 'scythe start')
     parser.add_argument('--num_workers', '-w', help = 'The number of workers.', required = True, type = int)
+    parser.add_argument('--clean', '-c', default = False, help = 'Whether to clean previous rq status.', action = 'store_true')
     args = parser.parse_args()
+
+    if args.clean:
+        for key in redis.keys('rq*'): redis.delete(key)
 
     processes = []
     for n in range(args.num_workers):
