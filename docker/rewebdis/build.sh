951a813d3dadb2f9acf687b0ea6c2cc03b4f13f5
Massimo Santini <massimo.santini@gmail.com>
2017-06-21 13:33:44 +0200

    Removed explicit Dockerfile name

--- docker/rewebdis/build.sh
+++ docker/rewebdis/build.sh
@@ -5,7 +5,7 @@ export REPO=scythe/rewebdis
 export TAG='latest'
 export COMMIT=$(git rev-parse HEAD | cut -b -8)
 
-docker build -f Dockerfile -t $REPO:$COMMIT .
+docker build -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:$VERSION
 if [ ! -z $1 ]; then
