57b3b36080f542092c55570b7bed6721ae85da15
Massimo Santini <massimo.santini@gmail.com>
2017-06-21 13:34:50 +0200

    Removed unprivileged user, sf  downloaded out of the Dockerfile

--- docker/testrunner/build.sh
+++ docker/testrunner/build.sh
@@ -10,11 +10,18 @@ export COMMIT=$(git rev-parse HEAD | cut -b -8)
 last_release_url=$(curl -sLo /dev/null -w '%{url_effective}' "https://github.com/scythe-suite/sim-fun-i/releases/latest")
 SIM_FUN_I_VERSION="${last_release_url##*/}"
 
-docker build -f Dockerfile --build-arg userid="$(id -u)" --build-arg version="$SIM_FUN_I_VERSION" -t $REPO:$COMMIT .
+if [ ! -r sf ]; then
+    curl -sLO "https://github.com/scythe-suite/sim-fun-i/releases/download/$SIM_FUN_I_VERSION/sf"
+fi
+
+docker build -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:$VERSION
 if [ ! -z $1 ]; then
     docker push $REPO
 fi
 
-rm -f st
+rm -f sf st
+
+echo "st tool: $(docker run -t --rm scythe/testrunner version)"
+echo "sf tool: $(docker run -t --rm --entrypoint sf scythe/testrunner version)"
