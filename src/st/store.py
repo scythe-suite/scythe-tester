7c92902ebb7e369e875ec30ef1bdd95cf151c8de
Massimo Santini <massimo.santini@gmail.com>
2017-10-27 14:51:32 +0200

    Update FORMAT and removed from code

--- src/st/store.py
+++ src/st/store.py
@@ -1,17 +1,3 @@
-"""
- SET	uids:<SESSION_ID> -> JSON({uid : <UID>, info : <INFO>, status: <STATUS>})
-HASH	texts:<SESSION_ID> -> <EXERCISE_NAME>: JSON({name: <TEXT_NAME>, content: <MARKDOWN_TEXT>})
-HASH	cases:<SESSION_ID> -> <EXERCISE_NAME>: JSON([{name: <CASE_NAME>, args: <ARGS>, input: <INPUT>, expected: <EXPECTED>}+])
-
-ZSET	timestamps:<SESSION_ID>:<UID> -> TIMESTAMP
-
-HASH	solutions:<UID>:<TIMESTAMP> -> <EXERCISE_NAME>: JSON([{content: <SOURCE_CODE>, name: <FILE_NAME>}+])
-HASH	results:<UID>:<TIMESTAMP> -> <EXERCISE_NAME>: JSON([{name: <CASE_NAME>, diffs: <ARGS>, errors: <INPUT>}+])
-HASH	compilations:<UID>:<TIMESTAMP> -> <EXERCISE_NAME>: <COMPILER_OUTPUT>
-
-HASH	summary:<SESSION_ID> -> <UID>: JSON([{name: <EXERCISE_NAME>, compile: <BOOLEAN>: diffs: <NUM_DIFFS>, errors: <NUM_ERRORS>, …}+])
-"""
-
 from base64 import decodestring, encodestring
 from json import loads, dumps
 from logging import Handler, Formatter, getLogger, INFO, WARN
