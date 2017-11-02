e9667fa7e195a73ec38102484610d26791dbc75a
Massimo Santini <massimo.santini@gmail.com>
2017-11-02 14:21:50 +0100

    Fixed a serious build problem

--- src/st/__init__.py
+++ src/st/__init__.py
@@ -12,7 +12,7 @@ LOG_LEVEL = INFO
 basicConfig(format = '%(asctime)s %(levelname)s: %(message)s', datefmt = '%Y-%m-%d %H:%M:%S', level = LOG_LEVEL)
 LOGGER = getLogger(__name__)
 
-VERSION = '0.5.1'
+VERSION = '0.5.2'
 
 TEST_UID = '000000' # see scythe/bin/scythe-prepare
 
